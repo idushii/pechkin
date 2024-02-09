6d99710f870fe7073cf557201526611c01be82ba
Nicolay I <nic.ilinov@gmail.com>
2024-02-09 18:30:56 +0700

    Процесс добавления раздела навигации

--- out/extension.js
+++ out/extension.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = void 0;
 const vscode_1 = require("vscode");
 const HelloWorldPanel_1 = require("./panels/HelloWorldPanel");
+const tree_1 = require("./utilities/tree");
 function activate(context) {
     // Create the show Pechkin command
     const showHelloWorldCommand = vscode_1.commands.registerCommand("pechkin.showHelloWorld", () => {
@@ -12,4 +13,8 @@ function activate(context) {
     context.subscriptions.push(showHelloWorldCommand);
 }
 exports.activate = activate;
+const rootPath = vscode_1.workspace.workspaceFolders && vscode_1.workspace.workspaceFolders.length > 0
+    ? vscode_1.workspace.workspaceFolders[0].uri.fsPath
+    : undefined;
+vscode_1.window.registerTreeDataProvider("nodeDependencies", new tree_1.NodeDependenciesProvider(rootPath));
 //# sourceMappingURL=extension.js.map
